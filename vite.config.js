9fc05857fde50909cd1e20c58ac033162b3c528c
Kallie Huynh <kalliehuynh@Kallies-MBP.ht.home>
2024-08-19 21:46:23 -0700

    Add build files for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/seagulley.github.io/' : '/',
+  base: '/',
   build: {
     outDir: 'docs'
   },
